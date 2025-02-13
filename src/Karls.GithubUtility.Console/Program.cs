b4013ce88aceb9b4b49e0cd4801b8541c3da546c
Karl-Johan Sjögren <karl-johan.sjogren@xlent.se>
2025-02-13 16:37:22 +0100

    Started on issue import stuff

--- src/Karls.GithubUtility.Console/Program.cs
+++ src/Karls.GithubUtility.Console/Program.cs
@@ -1,14 +1,19 @@
-﻿using Humanizer;
+﻿using System.IO.Abstractions;
+using Karls.GithubUtility.Console.Commands;
+using Karls.GithubUtility.Console.Commands.JiraIssueMigration;
+using Karls.GithubUtility.Console.Infrastructure;
+using Karls.GithubUtility.Core.Contracts;
+using Karls.GithubUtility.Core.Services;
 using Karls.GitHubUtility.Core.Octokit;
 using Karls.GitHubUtility.Core.Utilities;
+using Microsoft.Extensions.DependencyInjection;
 using Octokit;
 using Octokit.Internal;
 using Spectre.Console;
+using Spectre.Console.Cli;
 
 AnsiConsole.Clear();
 AnsiConsole.Write(new FigletText("Karls GitHub Utility").Color(Color.Green));
-
-var timeProvider = TimeProvider.System;
 
 ICredentialStore credentialStore;
 if(TokenHelper.HasToken()) {
@@ -19,7 +24,7 @@ if(TokenHelper.HasToken()) {
     var token = AnsiConsole.Ask<string>("Enter your access token").Trim();
     if(string.IsNullOrWhiteSpace(token)) {
         AnsiConsole.MarkupLine("[bold red]No token entered, exiting.[/]");
-        return;
+        return 1;
     }
 
     credentialStore = new InMemoryCredentialStore(new Credentials(token));
@@ -28,112 +33,32 @@ if(TokenHelper.HasToken()) {
 
 var client = new GitHubClient(new ProductHeaderValue("karls-githubutility"), credentialStore);
 
-var user = await client.User.Current();
+var services = new ServiceCollection();
+services.AddLogging();
+services.AddSingleton<IGitHubClient>(client);
+services.AddSingleton<IFileSystem, FileSystem>();
+services.AddSingleton(TimeProvider.System);
 
-var projectFullName = AnsiConsole.Ask<string>("Enter repository name (e.g. owner/repo)").Trim();
-var owner = projectFullName.Split('/')[0];
-var repository = projectFullName.Split('/')[1];
+services.AddSingleton<IJiraExportParser, JiraExportParser>();
 
-var artifacts = new List<Artifact>();
+var registrar = new TypeRegistrar(services);
 
-await AnsiConsole
-    .Progress()
-    .AutoClear(true)
-    .Columns([
-        new TaskDescriptionColumn(),
-        new ProgressBarColumn(),
-        new PercentageColumn(),
-        new RemainingTimeColumn(),
-        new SpinnerColumn()
-    ])
-    .StartAsync(async ctx => {
-        var task = ctx.AddTask($"[green]Fetching artifact metadata from {projectFullName}[/]");
+var app = new CommandApp(registrar);
+app.Configure(config => {
+    config.AddCommand<RemoveExpiredArtifactsCommand>("remove-expired-artifacts")
+        .WithDescription("Remove expired artifacts from the specified repository.")
+        .WithExample("remove-expired-artifacts -r owner/repo");
 
-        var page = 0;
-        var pageSize = 100;
+    config
+        .AddBranch("jira-issue-migration", generate => {
+            generate.SetDescription("Migrate issues from JIRA to Github.");
 
-        ListArtifactsResponse? response;
-        do {
-            var request = new ListArtifactsRequest { PerPage = pageSize, Page = ++page };
-            response = await client.Actions.Artifacts.ListArtifacts(owner, repository, request);
-            if(response is null || response.Artifacts is null || response.Artifacts.Count == 0) {
-                break;
-            }
+            generate.AddCommand<AnalyzeExportCommand>("analyze-export")
+                .WithDescription("Analyze a JIRA export file.");
 
-            artifacts.AddRange(response.Artifacts);
+            generate.AddCommand<CreateUserMappingCommand>("user-mapping")
+                .WithDescription("Setup mapping for JIRA users to Github users.");
+        });
+});
 
-            task.MaxValue = response.TotalCount;
-            task.Increment(response.Artifacts.Count);
-        } while(response.Artifacts.Count >= pageSize);
-    });
-
-var expirationBreakdown = artifacts.GroupBy(x => x.Expired).ToDictionary(x => x.Key, x => x.Sum(x => x.SizeInBytes));
-
-var breakdownChart = new BreakdownChart()
-    .Width(110)
-    .UseValueFormatter((x, _) => x.Bytes().Humanize("#.##"));
-
-foreach(var (expired, size) in expirationBreakdown) {
-    breakdownChart.AddItem(expired ? "Expired" : "Active", size, expired ? Color.Red : Color.Green);
-}
-
-AnsiConsole.WriteLine();
-AnsiConsole.Write(breakdownChart);
-
-var grid = new Grid();
-
-grid.AddColumn();
-grid.AddColumn();
-grid.AddColumn();
-
-grid.AddRow(["[blue]Artifact name[/]", "[green]Active size[/]", "[red]Expired size[/]"]);
-
-var artifactNames = artifacts.Select(x => x.Name).Distinct().OrderBy(x => x).ToArray();
-
-foreach(var name in artifactNames) {
-    var activeSize = artifacts.Where(x => x.Name == name && !x.Expired).Sum(x => x.SizeInBytes);
-    var expiredSize = artifacts.Where(x => x.Name == name && x.Expired).Sum(x => x.SizeInBytes);
-
-    grid.AddRow([name, activeSize.Bytes().Humanize("#.##"), expiredSize.Bytes().Humanize("#.##")]);
-}
-
-AnsiConsole.WriteLine();
-AnsiConsole.Write(grid);
-
-var prompt = new SelectionPrompt<string>()
-        .Title("Which artifact do you [red]want to remove[/]?")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Move up and down to see all options)[/]")
-        .AddChoices(artifacts.Select(x => x.Name).Distinct().OrderBy(x => x).ToArray());
-AnsiConsole.WriteLine();
-var artifactName = AnsiConsole.Prompt(prompt);
-
-if(!AnsiConsole.Confirm("Are you sure?", false)) {
-    return;
-}
-
-await AnsiConsole
-    .Progress()
-    .Columns([
-        new TaskDescriptionColumn(),
-        new ProgressBarColumn(),
-        new PercentageColumn(),
-        new RemainingTimeColumn(),
-        new SpinnerColumn()
-    ])
-    .StartAsync(async ctx => {
-        var task = ctx.AddTask($"[red]Removing artifacts with name {artifactName}[/]");
-
-        var artifactsToRemove = artifacts.Where(x => x.Name == artifactName && x.Expired && x.CreatedAt < timeProvider.GetUtcNow().AddDays(-1)).ToArray();
-        task.MaxValue = artifactsToRemove.Length;
-
-        foreach(var item in artifactsToRemove) {
-            try {
-                await client.Actions.Artifacts.DeleteArtifact(owner, repository, item.Id);
-                task.Increment(1);
-            } catch(Exception ex) {
-                AnsiConsole.WriteException(ex);
-                break;
-            }
-        }
-    });
+return app.Run(args);
